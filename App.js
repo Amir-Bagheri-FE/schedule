c54ba7d55df1c7bb1cec9c64d796316f1fd29498
AmirBagheri <amiramb10@gmail.com>
2025-01-21 11:04:20 +0330

    buttons added for each!

--- App.js
+++ App.js
@@ -18,10 +18,28 @@ FormTask.addEventListener("submit", (e) => {
   let Tasktitle=document.getElementById('Title')
   let  description=document.getElementById('Description')
   let timeout = document.getElementById('time')
+  if(Tasktitle.value===''){
+    alert('please add a title')
+  }
+  else{
   //making new boxes for added plans
   const newPlan = document.createElement('div')
   mainColumn.append(newPlan)
   newPlan.classList.add('addedPlan')
+  // buttons for boxes
+   let iconsDiv=document.createElement('div')
+   iconsDiv.classList.add('iconsDiv')
+   newPlan.append(iconsDiv)
+   const trash=document.createElement('li')
+   const page=document.createElement('li')
+   const up=document.createElement('li')
+   const down=document.createElement('li')
+   trash.classList.add('fas', 'fa-trash')
+   page.classList.add('fas', 'fa-file-alt')
+   up.classList.add('fas', 'fa-caret-up')
+   down.classList.add('fas', 'fa-caret-down')
+
+   iconsDiv.append(trash,page,up,down)
   //inserting info in boxes
   let Title=document.createElement('h3')
   newPlan.append(Title)
@@ -32,6 +50,7 @@ FormTask.addEventListener("submit", (e) => {
   let time=document.createElement('p')
   newPlan.append(time)
   time.textContent=timeout.value+"🕛"
+}
 }
 else {alert('maximum number plans reached please remove one')}
 });
