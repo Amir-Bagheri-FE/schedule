bd19c4995e7745604a2586259029b44e3a5c9ce8
AmirBagheri <amiramb10@gmail.com>
2025-01-21 09:50:05 +0330

    plans adding to main column!

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const AddBox = document.getElementById("AddTask");
 const popUpForm = document.getElementById("popUpForm");
 const FormTask = document.querySelector("#FormTask");
-
+let mainColumn = document.getElementById('main')
 AddBox.addEventListener("click", () => {
   popUpForm.style.display = "flex";
   popUpForm.addEventListener("click", (e) => {
@@ -12,4 +12,26 @@ AddBox.addEventListener("click", () => {
 });
 FormTask.addEventListener("submit", (e) => {
   e.preventDefault();
+  popUpForm.style.display = "none";
+  //form inputs 
+  if(mainColumn.childElementCount<= 4){
+  let Tasktitle=document.getElementById('Title')
+  let  description=document.getElementById('Description')
+  let timeout = document.getElementById('time')
+  //making new boxes for added plans
+  const newPlan = document.createElement('div')
+  mainColumn.append(newPlan)
+  newPlan.classList.add('addedPlan')
+  //inserting info in boxes
+  let Title=document.createElement('h3')
+  newPlan.append(Title)
+  Title.textContent=Tasktitle.value
+  let describe=document.createElement('p')
+  newPlan.append(describe)
+  describe.textContent=description.value
+  let time=document.createElement('p')
+  newPlan.append(time)
+  time.textContent=timeout.value+"🕛"
+}
+else {alert('maximum number plans reached please remove one')}
 });
