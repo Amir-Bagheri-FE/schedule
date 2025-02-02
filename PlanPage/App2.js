253b35763bdb992647beedc037a64cbab5be9771
AmirBagheri <amiramb10@gmail.com>
2025-02-02 16:25:43 +0330

    timer circle

--- PlanPage/App2.js
+++ PlanPage/App2.js
@@ -33,9 +33,29 @@ function displayTimer() {
   let TimerSeconds = Seconds.value < 10 ? "0" + Seconds.value : Seconds.value;
   let TimerMinutes = Minutes.value < 10 ? "0" + Minutes.value : Minutes.value;
   let TimerHours = Hours.value < 10 ? "0" + Hours.value : Hours.value;
-
+  //displaying a time circle
+  let allTime =(TimerHours*3600)+(TimerMinutes*60)+TimerSeconds
+  const timecircle=document.createElement('div');
+  const timeInnercircle=document.createElement('div');
+  timecircle.classList.add('timecircle')
+  timeInnercircle.classList.add('timeInnercircle')
+  InfoDiv.append(timecircle)
+  timecircle.append(timeInnercircle)
+  //time in info div
   Timer.textContent = TimerHours + ":" + TimerMinutes + ":" + TimerSeconds;
   const interval = setInterval(function TimerCountDown() {
+       
+       const  currentTIme =(TimerHours*3600)+(TimerMinutes*60)+TimerSeconds
+       const timePercent=(currentTIme*100)/allTime;
+       if(timePercent<8 && timePercent>5.6){
+        timeInnercircle.style.top='17px'
+       }
+       else if (timePercent<5.5 && timePercent > 3.1){
+        timeInnercircle.style.top='20px'
+       }
+       else if (timePercent<3){
+        timeInnercircle.style.top='25px'
+       }
     if (Number(TimerSeconds) != 0) {
       TimerSeconds =
         TimerSeconds < 10 ? "0" + (TimerSeconds - 1) : TimerSeconds - 1;
@@ -60,8 +80,10 @@ function displayTimer() {
         } else {
           Timer.textContent = "time is over";
           clearInterval(interval);
+        timeInnercircle.style.top='30px'
+
         }
       }
     }
-  }, 1000);
+  }, 30);
 }
